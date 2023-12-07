8b9a4f6bff231c26b46294b6bcbe0d1a62038ee8
hawkup <spiritsquare@gmail.com>
2023-12-07 21:27:27 +0700

    build flecs

--- randomlib.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include "randomlib.h"
-
-int getRandomNumber(int min, int max) {
-    return rand() % (max - min + 1) + min;
-}
--- randomlib.h
+++
@@ -1,6 +0,0 @@
-#ifndef RANDOMLIB_H
-#define RANDOMLIB_H
-
-int getRandomNumber(int min, int max);
-
-#endif
